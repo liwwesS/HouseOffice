49e3ae019e029cabdea7b87c8d8a7e329c80a188
liwwesS <ka.novik@yandex.by>
2024-05-15 15:51:28 +0300

    added default value in Request

--- DAL/HouseOffice.DAL/ApplicationContext.cs
+++ DAL/HouseOffice.DAL/ApplicationContext.cs
@@ -9,6 +9,7 @@ namespace HouseOffice.DAL
         public DbSet<User> Users => Set<User>();
         public DbSet<Role> Roles => Set<Role>();
         public DbSet<Request> Requests => Set<Request>();
+        public DbSet<UserRequest> UserRequests => Set<UserRequest>();
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -17,6 +18,13 @@ namespace HouseOffice.DAL
                 new Role { Id = 2, RoleType = "Boss" },
                 new Role { Id = 3, RoleType = "Worker" },
                 new Role { Id = 4, RoleType = "User" });
+
+            modelBuilder.Entity<Request>().HasData(
+                new Request { Id = 1, RequestType = "На улучшение жилищных условий" },
+                new Request { Id = 2, RequestType = "Выдача справки о составе семьи" },
+                new Request { Id = 3, RequestType = "На обследование дома" },
+                new Request { Id = 4, RequestType = "На снятие дома с кадастрого учёта" },
+                new Request { Id = 5, RequestType = "Выдача справки о трудовом стаже" });
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
